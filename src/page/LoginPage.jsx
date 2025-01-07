9968fcdf188b7fe73c0cc644cff11700a34d6cc1
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-07 16:23:19 +0530

    bug fixed

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,29 +1,54 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const LoginPage = () => {
-  const [username,setUsername]=useState("");
-  const [password,setPassword]=useState("");
-  const [email,setEmail]=useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [isDisabled,setIdDisabled]=useState(false);
 
-  
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    handleOpen();
 
-    const data={
-      email:email,
-      password:password,
-      username:username
-    }
+    const data = {
+      email: email,
+      password: password,
+      name: username,
+    };
 
-    axios.post(import.meta.env.VITE_SERVER_URL+"auth/register")
-    
-    
-    
+    axios
+      .post(import.meta.env.VITE_SERVER_URL + "/auth/register", data)
+      .then((rsp) => {
+        handleClose();
+        setIdDisabled(true);
+        console.log(rsp);
+        
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Backdrop
+        sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <form
         onSubmit={handleSubmit}
         className="p-6 bg-white rounded-lg shadow-md w-[450px]"
@@ -31,23 +56,31 @@ const LoginPage = () => {
         <h2 className="text-2xl font-bold mb-4 text-center">Sign In</h2>
 
         <div className="mb-4">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700"
+          >
             Username
           </label>
           <input
-            type="text" 
+            type="text"
             id="username"
             name="username"
             value={username}
-            placeholder='username'
-            onChange={(e)=>{setUsername(e.target.value)}}
+            placeholder="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
             className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             required
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
             Email
           </label>
           <input
@@ -55,24 +88,31 @@ const LoginPage = () => {
             id="email"
             name="email"
             value={email}
-            placeholder='email@.com'
-            onChange={(e)=>{setEmail(e.target.value)}}
+            placeholder="email@.com"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             required
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
             Password
           </label>
           <input
             type="password"
             id="password"
             name="password"
-            placeholder='*******'
+            placeholder="*******"
             value={password}
-            onChange={(e)=>{setPassword(e.target.value)}}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             required
           />
@@ -80,8 +120,8 @@ const LoginPage = () => {
 
         <button
           type="submit"
-          
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+           disabled={isDisabled}
+          className={`w-full bg-blue-500 text-white py-2 px-4 rounded-md ${ isDisabled==false ? 'hover:bg-blue-600' :'bg-gray-400' } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50`}
         >
           Sign In
         </button>
